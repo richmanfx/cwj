7bf8835cb97691981a7bb6c865f6df9a91cbbc40
richmanfx <richmanfx@yandex.ru>
2020-08-12 21:28:37 +0300

    Конфигурационный файл. Логгер.

--- src/main/java/ru/r5am/CwJ.java
+++ src/main/java/ru/r5am/CwJ.java
@@ -1,23 +1,36 @@
 package ru.r5am;
 
-import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import org.apache.logging.log4j.Logger;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
 
 public class CwJ extends Application {
 
+    protected static final Logger log = LogManager.getLogger();
+    protected static final CwjConfig config = ConfigFactory.create(CwjConfig.class);
+
     public static void main(String[] args) {
+        log.info("Start!");
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
-        String programTitle = "CwJ";
+        String programTitle = "CWJ";
+
+        // Установка размеров главной формы
+        primaryStage.setMinWidth(config.minimumWindowWidth());
+        primaryStage.setMinHeight(config.minimumWindowHeight());
+        primaryStage.setMaxWidth(config.maximumWindowWidth());
+        primaryStage.setMaxHeight(config.maximumWindowHeight());
 
         Group group = new Group();
-        Scene scene = new Scene(group, 600, 500);
+        Scene scene = new Scene(group, config.startWindowWidth(), config.startWindowHeight());
 
         primaryStage.setTitle(programTitle);
         primaryStage.setScene(scene);
---
+++ src/main/java/ru/r5am/CwjConfig.java
@@ -0,0 +1,21 @@
+package ru.r5am;
+
+import org.aeonbits.owner.Config;
+
+/**
+ * Чтение параметров из файла конфигурации
+ */
+@Config.Sources({"file:cwj.config"})
+public interface CwjConfig extends Config {
+
+    // Максимальные и минимальные размеры главного окна в пикселях
+    Integer maximumWindowHeight();
+    Integer maximumWindowWidth();
+    Integer minimumWindowHeight();
+    Integer minimumWindowWidth();
+
+    Integer startWindowHeight();
+    Integer startWindowWidth();
+
+}
+
