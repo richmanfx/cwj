3f5b75f56a8a81b136ea172a3911a97941fdb07b
richmanfx <richmanfx@yandex.ru>
2020-10-11 18:02:08 +0300

    Выбор и чтение файла с CW словами

--- src/main/java/ru/r5am/controllers/MainController.java
+++ src/main/java/ru/r5am/controllers/MainController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,12 +12,15 @@ import ru.r5am.utils.FilesWork;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class MainController {
 
     static final Logger log = LogManager.getLogger();
     Map<String, String> symbolToCw;     // Соответствие символов посылкам
+    List<String> cwWords = new ArrayList<>();
 
     // Хелп
 //    OHJHelp cwjHelp = new OHJHelp();
@@ -38,6 +42,8 @@ public class MainController {
     // Привязка переменных к компонентам в cwj.fxml
     @FXML private Button buttonExit;
     @FXML private Button buttonFileSelect;
+    @FXML private VBox mainVBox;
+
 
     /**
      *  Обработка нажатий мышкой на Buttons (клавиатура отдельно обрабатывается!)
@@ -64,19 +70,14 @@ public class MainController {
                 currentWindowClose(actionEvent);    // Закрыть текущее окно
                 break;
 
-                // Кнопка "Выбрать файл слов" на главном окне
+            // Кнопка "Выбрать файл слов" на главном окне
             case "buttonFileSelect":
-                wordsFileSelect();
+                cwWords = FilesWork.wordsFileRead(mainVBox);
+                System.out.println(cwWords);
+                log.info("CW words: {}", cwWords);
                 break;
 
         }
-
-    }
-
-    /**
-     * Выбрать файл CW слов
-     */
-    private void wordsFileSelect() throws IOException {
 
     }
 
--- src/main/java/ru/r5am/utils/FilesWork.java
+++ src/main/java/ru/r5am/utils/FilesWork.java
@@ -1,12 +1,18 @@
 package ru.r5am.utils;
 
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.aeonbits.owner.ConfigFactory;
 import ru.r5am.CwjConfig;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +21,7 @@ public class FilesWork {
 
     static final CwjConfig config = ConfigFactory.create(CwjConfig.class);
     static String resourcePath = "src/main/resources/";
+
 
     /**
      * Прочитать файл соответствия символов посылкам
@@ -34,6 +41,41 @@ public class FilesWork {
         }
 
         return symbolToCw;
+    }
+
+    /**
+     * Выбрать файл CW слов, прочитать его, вернуть слова
+     */
+    public static List<String> wordsFileRead(VBox vBox) throws IOException {
+
+        List<String> cwWords = new ArrayList<>();
+        final FileChooser fileChooser = new FileChooser();
+        Stage stage = (Stage) vBox.getScene().getWindow();
+
+        File file = fileChooser.showOpenDialog(stage);
+
+
+        if (file != null) {
+            //
+            Path path = file.toPath();
+            cwWords = Files.readAllLines(path);
+
+        } else {
+            // Сообщение об отсутствии файла в окно сообщений
+            // TODO:
+
+        }
+
+        return cwWords;
+    }
+
+    private static void openFile(File file) {
+        Desktop desktop = Desktop.getDesktop();
+        try {
+            desktop.open(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
