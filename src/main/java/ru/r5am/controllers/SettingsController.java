ed684663798466869635f26f2cc93808c207f64e
richmanfx <richmanfx@yandex.ru>
2021-05-15 11:03:01 +0300

    Обработка кнопки "Отмена" формы настроек

--- src/main/java/ru/r5am/controllers/SettingsController.java
+++ src/main/java/ru/r5am/controllers/SettingsController.java
@@ -2,29 +2,37 @@ package ru.r5am.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import org.aeonbits.owner.ConfigFactory;
 import ru.r5am.CwjConfig;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-
 public class SettingsController {
 
     static final CwjConfig config = ConfigFactory.create(CwjConfig.class);
 
-    @FXML private CheckBox rusSymbolsCheckBox;
-    @FXML private CheckBox engInterfaceCheckBox;
-    @FXML private CheckBox notRandomWordsCheckBox;
-    @FXML private TextField startPauseTextField;
-    @FXML private TextField caliberCwSpeedTextField;
-    @FXML private TextField wordQuantityTextField;
-    @FXML private TextField cwSpeedTextField;
-    @FXML private TextField toneTextField;
-    @FXML private TextField intervalTextField;
+    @FXML
+    private CheckBox rusSymbolsCheckBox;
+    @FXML
+    private CheckBox engInterfaceCheckBox;
+    @FXML
+    private CheckBox notRandomWordsCheckBox;
+    @FXML
+    private TextField startPauseTextField;
+    @FXML
+    private TextField caliberCwSpeedTextField;
+    @FXML
+    private TextField wordQuantityTextField;
+    @FXML
+    private TextField cwSpeedTextField;
+    @FXML
+    private TextField toneTextField;
+    @FXML
+    private TextField intervalTextField;
 
-    @FXML private void initialize() {
+    @FXML
+    private void initialize() {
 
         // Заполнить поля формы настроек значениями из конфигурационного файла
         rusSymbolsCheckBox.setSelected(config.rusSymbols());
@@ -40,23 +48,49 @@ public class SettingsController {
     }
 
 
-    public void buttonProcessing(ActionEvent actionEvent)
-            throws NoSuchMethodException, InstantiationException, IllegalAccessException, IOException,
-            InvocationTargetException {
+    public void buttonProcessing(ActionEvent actionEvent) {
 
+        Object source = actionEvent.getSource();        // Определить источник нажатия
+
+        // Если источник события кнопка, то что-то делать
+        if ((source instanceof Button)) {
+
+            Button clickedButton = (Button) source;     // Нисходящее приведение
+
+            // По ID определяем конкретную кнопку
+            switch (clickedButton.getId()) {
+
+                case "saveButton":
+                    // TODO: Сохранять параметры в конфиг-файл
+
+                    // Проверить правильность введённых значений
+
+                    // Сохранить параметры в конфиг-файл
+
+                    // Закрыть форму настроек
+
+                    // Перечитать параметры из конфиг-файла ???
+
+                    break;
+
+                case "cancelButton":
+                    // Выход без сохранения параметров
+                    MainController.currentWindowClose(actionEvent);      // Закрыть текущее окно
+                    break;
+            }
+        }
     }
 
+//    // Проверка правильности введённых значений
+//    private String valueValidation() {
+//
+//        return "";
+//
+//    }
 
-    // Проверка правильности введённых значений
-    private String valueValidation() {
 
-        return "";
-
-    }
-
-
-    private void settingsSave() {
-
-    }
+//    private void settingsSave() {
+//
+//    }
 
 }
